907f170936b26c1996912f72b06585d30642be79
Hantarinh0 <chrismine9@gmail.com>
2023-04-10 18:58:35 -0300

    Mudanças na Js e comentários

--- src/main/java/br/edu/ufrb/gcet236/enfermeiros/EnfermeirosApplication.java
+++ src/main/java/br/edu/ufrb/gcet236/enfermeiros/EnfermeirosApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnfermeirosApplication {
-
+	
+	// Método principal que inicia a aplicação
 	public static void main(String[] args) {
 		SpringApplication.run(EnfermeirosApplication.class, args);
 	}
