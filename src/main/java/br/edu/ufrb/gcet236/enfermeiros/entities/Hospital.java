16c177770b0011dcf4c7bbbb0edf66215f731001
Hantarinh0 <chrismine9@gmail.com>
2023-04-10 18:42:26 -0300

    Adicionando cometários

--- src/main/java/br/edu/ufrb/gcet236/enfermeiros/entities/Hospital.java
+++ src/main/java/br/edu/ufrb/gcet236/enfermeiros/entities/Hospital.java
@@ -4,54 +4,41 @@ import java.util.ArrayList;
 
 public class Hospital {
     private ArrayList<Pessoa> colaboradores = new ArrayList<Pessoa>();
-
+    
+    //retorna a lista de colaboradores cadastrados no hospital.
     public ArrayList<Pessoa> getColaboradores() {
         return this.colaboradores;
     }
-
+    
+    //adiciona um objeto do tipo Pessoa à lista de colaboradores cadastrados no hospital.
     public void cadastrarColaboradores(Pessoa colaborador) {
         this.colaboradores.add(colaborador);
     }
     
-    public void editarColaboradores(Pessoa enfermeiro, String cpfAntigo){
-       
-        for (int i = 0; i < colaboradores.size(); i++ ) {
-            Pessoa colaborador = colaboradores.get(i);
-            String cpfDoColaborador = colaborador.getCpf();
-            if (cpfAntigo.equalsIgnoreCase(cpfDoColaborador)) {
-                colaboradores.set(i, enfermeiro);
-            }
-        }
-    }
-
-    public void removerColaboradores(Pessoa colaborador) {
-        this.colaboradores.remove(colaborador);
-    }
-
+    //busca na lista de colaboradores por nome, ignorando o caso, e retorna uma lista de objetos do tipo Pessoa que possuem o nome informado.
     public ArrayList<Pessoa> buscarPorNome(String nome) {
-        // Todo Baseado nos Exercícios 5.3 de 14 Mar no ClassRoom
         ArrayList<Pessoa> resultados = new ArrayList<Pessoa>();
         for (Pessoa colaborador : this.colaboradores) {
-            if (colaborador.getNome().contains(nome)) {
+            if (nome.equalsIgnoreCase(colaborador.getNome())) {
                 resultados.add(colaborador);
             }
         }
         return resultados;
     }
     
+    // busca na lista de colaboradores por CPF, ignorando o caso, e retorna uma lista de objetos do tipo Pessoa que possuem o CPF informado.
     public ArrayList<Pessoa> buscarPorCPF(String cpf) {
         ArrayList<Pessoa> resultados = new ArrayList<Pessoa>();
         for (Pessoa colaborador : this.colaboradores) {
-            String cpfDoColaborador = colaborador.getCpf();
-            if (cpf.equalsIgnoreCase(cpfDoColaborador)) {
+            if (cpf.equalsIgnoreCase(colaborador.getCpf())) {
                 resultados.add(colaborador);
             }
         }
         return resultados;
     }
     
+    //busca na lista de colaboradores por RG, ignorando o caso, e retorna uma lista de objetos do tipo Pessoa que possuem o RG informado
     public ArrayList<Pessoa> buscarPorRG(String rg) {
-        // Todo Baseado nos Exercícios 5.3 de 14 Mar no ClassRoom
         ArrayList<Pessoa> resultados = new ArrayList<Pessoa>();
         for (Pessoa colaborador : this.colaboradores) {
             if (rg.equalsIgnoreCase(colaborador.getRg())) {
@@ -60,20 +47,9 @@ public class Hospital {
         }
         return resultados;
     }
-
-    public ArrayList<Pessoa> buscarPorTelefone(String telefone) {
-        // Todo Baseado nos Exercícios 5.3 de 14 Mar no ClassRoom
-        ArrayList<Pessoa> resultados = new ArrayList<Pessoa>();
-        for (Pessoa colaborador : this.colaboradores) {
-            if (telefone.equalsIgnoreCase(colaborador.getTelefone())) {
-                resultados.add(colaborador);
-            }
-        }
-        return resultados;
-    }
-   
+    
+    //busca na lista de colaboradores por lotação, ignorando o caso, e retorna uma lista de objetos do tipo Pessoa que possuem a lotação informada. 
     public ArrayList<Pessoa> buscarPorLotação(String lotação) {
-        System.out.println("lotação: " + lotação);
         ArrayList<Pessoa> resultados = new ArrayList<Pessoa>();
         for (Pessoa colaborador : this.colaboradores) {
             if (colaborador instanceof Enfermeiro) {
